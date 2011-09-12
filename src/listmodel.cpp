247494b9bdf136ea48da80f4986d0864df864a69
Dominik Koeppl <dominik@devwork.org>
2011-09-12 23:22:50 +0200

    changed text-output in listmodel - thanks to tosnic

--- src/listmodel.cpp
+++ src/listmodel.cpp
@@ -91,32 +91,45 @@ int ListModel::daysLeft(const QModelIndex & index) const
 
 static QString formatComment(Anniv::Type type, int daysleft)
 {
+	/* %n daysleft
+	 * %1 Name
+	 * %2 Date
+	 * %3 optional: yeardiff
+	 */
+
 	switch (type)
 	{
+	case Anniv::ANNIVERSARY:
+		return QObject::tr("In %n day(s) is %1, exactly at %2.", "", daysleft);
 	case Anniv::BIRTHDAY:
 		return QObject::tr("In %n day(s) %1 has birthday, exactly at %2.", "", daysleft);
 	case Anniv::DEATHDAY:
 		return QObject::tr("In %n day(s) %1 died, exactly at %2.", "", daysleft);
-	case Anniv::MEMORIAL:
-		return QObject::tr("In %n day(s) it's %1 memorial, exactly at %2.", "", daysleft);
+	case Anniv::NAMESDAY:
+		return QObject::tr("In %n day(s) %1 has namesday, exactly at %2.", "", daysleft);
+	case Anniv::WEDDING:
+		return QObject::tr("In %n day(s) is %1 %3th wedding day, exactly at %2.", "", daysleft);
 	default:
 		return QObject::tr("In %n day(s) is %1, exactly at %2.", "", daysleft);
 	}
 }
 
-static QString formatCommentWithYear(Anniv::Type type, int daysleft, int years)
+static QString formatCommentWithYear(Anniv::Type type, int daysleft)
 {
 	switch (type)
 	{
 	case Anniv::BIRTHDAY:
-		return QObject::
-			tr("In %n day(s) %2 has the %1th birthday, exactly at %3.", "", daysleft).arg(years);
+		return QObject::tr("In %n day(s) %1 becomes %3, %1 was born on %2.", "", daysleft);
+	case Anniv::ANNIVERSARY:
+		return QObject::tr("In %n day(s) is %1 for the %3th time. This day originated on %2.","", daysleft);
 	case Anniv::DEATHDAY:
-		return QObject::
-			tr("In %n day(s) %2 died for %1 years, exactly at %3.", "", daysleft).arg(years);
-
+		return QObject::tr("In %n day(s) %1 is dead for %3 years. %1 died on %3.", "", daysleft);
+	case Anniv::NAMESDAY:
+		return QObject::tr("In %n day(s) %1 has namesday. This day originated on %2, thus is %3 ages old.", "", daysleft);
+	case Anniv::WEDDING:
+		return QObject::tr("In %n day(s) is %1 %3th wedding day. %1 is married since %2.", "", daysleft);
 	default:
-		return formatComment(type, daysleft) + QObject::tr(" (%3 year(s))", "", years);
+		return QObject::tr("In %n day(s) is %1 for the %3th time. This day originated on %2.", "", daysleft);
 	}
 }
 
@@ -143,12 +156,13 @@ QVariant ListModel::data(const QModelIndex & index, int role) const
 
 
 		if (hasYear)
-			format = formatCommentWithYear(type, daysleft, QDate::currentDate().year() - date.year());
+			format = formatCommentWithYear(type, daysleft);
 		else
 			format = formatComment(type, daysleft);
 
 		format = format.arg(model->data(model->index(row, TableModel::COLUMN_NAME)).toString())
 						.arg(hasYear ? date.toString(Qt::SystemLocaleLongDate) : date. toString(tr("dddd, d MMMM")));
+		if(hasYear) format = format.arg(QDate::currentDate().year() - date.year());
 
 		return format;
 
